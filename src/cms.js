f735ed5f0693534be0828668de9993e8330190be
zeus-dev919 <cccodechef@gmail.com>
2019-11-22 14:36:41 +0100

    add tracking header

--- src/cms.js
+++ src/cms.js
@@ -4,6 +4,12 @@ const PROJECT_ID = 'f71ad6dd-59c3-002a-7dc9-5bbf5521f1a3';
 
 const deliveryClient = new DeliveryClient({
   projectId: PROJECT_ID,
+  globalHeaders: [
+    {
+      header: 'X-KC-SOURCE',
+      value: 'kontent-sample-app-svelte-sapper;1.0.0'
+    }
+  ]
 });
 
 export async function getProducts() {
